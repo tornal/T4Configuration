d1522145e2f4ad38d859ff5f3063f2d4e26b7bc3
tornal <spam@sineshift.co.uk>
2011-03-25 16:16:15 +0000

    - Generates all app settings and connection settings into a static class

---
+++ ConfigurationTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace ConfigurationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+            if (appSettings.Count > 0)
+            {
+                for (int i = 0; i < appSettings.Count; i++)
+                {
+                    Console.WriteLine("Key: {0}", appSettings.GetKey(i));
+                    Console.WriteLine("Value: {0}", appSettings.GetValues(i));
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+
+}
---
+++ Sineshift.Configuration/StrongTypeConfiguration.generated.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+
+namespace Sineshift.Configuration
+{
+	public static class Config
+	{
+	
+		public static String ClientValidationEnabled
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["ClientValidationEnabled"];
+			}
+		}
+	
+		public static String UnobtrusiveJavaScriptEnabled
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["UnobtrusiveJavaScriptEnabled"];
+			}
+		}
+		public static String ApplicationServices
+		{
+			get
+			{
+				return ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+			}
+		}
+		public static String MisAdminWebEntities
+		{
+			get
+			{
+				return ConfigurationManager.ConnectionStrings["MisAdminWebEntities"].ConnectionString;
+			}
+		}
+		public static String MisDataContext
+		{
+			get
+			{
+				return ConfigurationManager.ConnectionStrings["MisDataContext"].ConnectionString;
+			}
+		}
+	}
+}
+
