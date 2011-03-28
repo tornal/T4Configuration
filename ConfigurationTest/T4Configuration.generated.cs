56ccf58f3df0e8596ba35437d0499c84ea31615b
tornal <spam@sineshift.co.uk>
2011-03-28 11:52:11 +0100

    - Working NuGet package, installs and adds references correctly

---
+++ ConfigurationTest/T4Configuration.generated.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConfigurationTest
+{
+	public static class Config
+	{
+	
+		public static String Setting1
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["Setting1"];
+			}
+		}
+	
+		public static String Setting2
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["Setting2"];
+			}
+		}
+	}
+}
+
